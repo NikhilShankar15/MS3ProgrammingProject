0ce75122eb5ca6b411a857372a55c95b3e40d325
nshank3 <nshank3@uic.edu>
2018-06-04 01:29:07 -0400

    Code complete?
    Fixed bugs, added comments
    ReadMe requires updates

--- src/Migrate_Data.java
+++ src/Migrate_Data.java
@@ -4,55 +4,83 @@ import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 
+
 public class Migrate_Data {
     public static void main(String[] args) throws FileNotFoundException {
-        //Create connection to DB
+        /**Create and Establish Connection to DB
+         * Make Bad Data File
+         * Establish Records
+         * Uses sqlite-jdbc-3.23.1.jar as driver, must be added to classpath
+         **/
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         PrintWriter pw = new PrintWriter(new File("bad-data-<"+timeStamp+">.csv"));
         Connection con = null;
         int records_recieved=0;
         int records_succesful=0;
         int records_failed=0;
-        int row = 1; //For debugging
-        boolean cont=true;
+        int row = 1; //Keep track of which row in Database is being processed(debugging purposes)
+        boolean cont=true; //Boolean for while loop
         try {
-            String url = "jdbc:sqlite:src/identifier.sqlite";
+            String url = "jdbc:sqlite:src/DataBase.sqlite";//creates database if none exists in src folder
             con = DriverManager.getConnection(url);
             System.out.println("Connection Succesful");
+            String sql_table = "CREATE TABLE IF NOT EXISTS Fields (\n"
+                            + " A TEXT,\n"
+                            + " B TEXT,\n"
+                            + " C TEXT,\n"
+                            + " D TEXT,\n"
+                            + " E TEXT,\n"
+                            + " F TEXT,\n"
+                            + " G TEXT,\n"
+                            + " H TEXT,\n"
+                            + " I TEXT,\n"
+                            + " J TEXT\n"
+                            + ");";
+            Statement stmt = con.createStatement();
+            stmt.execute(sql_table);
+            System.out.println("Table Fields Created");
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
 
         Scanner scanner = new Scanner(new File("src/ms3interview.csv"));
         scanner.useDelimiter("\n");//Parse into lines
-        scanner.next(); //Remove Header
-
+        scanner.next();
         //Parse through csv
         while(cont) {
-            //System.out.println(row);
-            boolean b = true;//Send to DB condition
+            boolean b = true;//Send to DB if condition is true
             String input = scanner.next();
             records_recieved++;
-            List<String> InputArray = new LinkedList<String>(Arrays.asList(input.split(",",-1)));
-            //System.out.println(InputArray);
-            //Ensure Image Address is in one entry
+            List<String> InputArray = new LinkedList<>(Arrays.asList(input.split(",",-1)));
             int size =InputArray.size();
-            //check if any column has no entry
+            /**
+             * Assumption 1: All properly formated records
+             * will have an image address begining with "data:image/png,";
+             * consequently split will seperate the entire address into two seperate columns
+             * Use Two checks to ensure record is valid: no non-empty columns and 11 columns total
+             **/
             for(int i=0;i<size;i++){
-                if (InputArray.get(i).isEmpty())b=false;
+                if (InputArray.get(i).isEmpty())b=false; //if any column is empty, do not send
             }
-            if(InputArray.size()!=11)b=false;
+            if(size!=11)b=false; //Check size
 
             if (b) {
                 //concatonate image address
                 String temp = "\"" + InputArray.get(4) + InputArray.get(5)+"\"";
-
-                InputArray.set(4,temp);
+                /** Assumption 2: Column D will always contain an Image address
+                 *
+                 * Assumption3: Valid records will be split incorrectly at this point, must concatonate
+                 * columns 4 and 5 for for a proper entry
+                 */
                 InputArray.remove(5);
+                InputArray.set(4,temp);
+                System.out.println(row);
                 String sql = "INSERT INTO Fields(A,B,C,D,E,F,G,H,I,J) VALUES(?,?,?,?,?,?,?,?,?,?)";
 
 
@@ -71,14 +99,14 @@ public class Migrate_Data {
                 } catch (SQLException e) {
                     System.out.println(e.getMessage());
                 }
-            records_succesful++;
+            records_succesful++; //If we have reached this point we have succesfully inserted into DB
             }
             else {
                 pw.write(input+"\n");
-                records_failed++;//Ensure records failed is incremented
+                records_failed++;// else Records failed is incremented
             }
             row++;
-        if(scanner.hasNext()==false) cont = false;
+        if(!scanner.hasNext()) cont = false;
         }
         scanner.close();
         pw.close();
