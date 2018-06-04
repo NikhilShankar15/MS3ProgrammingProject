0b74b9ef0c337261ebd06fd08efd3729cbff5603
nshank3 <nshank3@uic.edu>
2018-06-04 09:45:39 -0400

    Create Database no longer optional

--- src/Migrate_Data.java
+++ src/Migrate_Data.java
@@ -31,7 +31,6 @@ public class Migrate_Data {
                     + ");";
             Statement stmt = con.createStatement();
             stmt.execute(sql_table);
-            System.out.println("Table Fields Created");
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
